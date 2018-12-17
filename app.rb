c8195cdf285e0df7230d2d9acc8acc4ab5597b91
Leifur Sigurdsson <leifurerik@gmail.com>
2018-12-17 23:32:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,12 +8,13 @@ class App < Sinatra::Base
 
   post '/team' do
     @name = params[:name]
-    @coach = params[:couch]
+    @coach = params[:coach]
     @pg = params[:pg]
     @sg = params[:sg]
-    @sf = params[:sf]
     @pf = params[:pf]
+    @sf = params[:sf]
     @c = params[:c]
     erb :team
   end
+
 end
