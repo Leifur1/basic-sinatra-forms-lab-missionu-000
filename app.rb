3e771dfa7ccf16e00f8056b5665972ee3003086b
Leifur Sigurdsson <leifurerik@gmail.com>
2018-12-17 23:22:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,9 +6,14 @@ class App < Sinatra::Base
     erb :newteam
   end
 
-  post '/team' do
+  post '/team/:teamname/:coach/:pg/:sg/:sf/:pf/:c' do
+    @teamname = params[:teamname].to_s
+    @coach = params[:couch].to_s
+    @pg = params[:pg].to_s
+    @sg = params[:sg].to_s
+    @sf = params[:sf].to_s
+    @pf = params[:pf].to_s
+    @c = params[:c].to_s
     erb :team
   end
-
-
 end
